7f8c889eea580392eae09eecc669c2207cf8d891
makpevwei <makpevwei@yahoo.com>
2025-02-02 20:33:46 +0100

    Update to code

---
+++ password_generator_web.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+from password_generator_cli import generate_password
+from file_operations import read_passwords_from_file, write_password_to_file
+
+
+st.title("Passowrd Generator App")
+st.write("Generate a secure password it to a file")
+
+n_letters = st.number_input("Number of letters", min_value=0,  value=3)
+n_numbers = st.number_input("Number of numbers", min_value=0, value=3)
+n_symbols = st.number_input("Number of symbols", min_value=0, value=3)
+
+if st.button("Generate Passord"):
+    password = generate_password(n_letters,n_numbers,n_symbols)
+    
+    write_password_to_file(password)
+    st.info("Password saved to file")
+    
+if st.checkbox("Show Saved Passwords"):
+    passwords = read_passwords_from_file()
